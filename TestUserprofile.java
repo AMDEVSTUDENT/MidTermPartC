42b6a0ce6cdc0db05dca396d2cec9c9f719f63c0
AMDEVSTUDENT <124113000+AMDEVSTUDENT@users.noreply.github.com>
2023-02-21 14:20:56 -0500

    Add files via upload

---
+++ TestUserprofile.java
@@ -0,0 +1,9 @@
+public class TestUserprofile {
+    int x = 5;
+  
+    public static void main(String[] args) {
+        TestUserprofile myObj = new Main();
+      System.out.println(myObj.x);
+    }
+  }
+  
